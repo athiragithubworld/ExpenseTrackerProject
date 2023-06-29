4d5a18a29b741f5574919942891accf35ce55377
athiragithubworld <athirahrms@gmail.com>
2023-06-29 19:14:55 +0530

    App js file commited

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LoginPage from "./Components/Authentication/LoginPage";
+import Header from "./Components/Layouts/Header";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <LoginPage></LoginPage>
+    </div>
+  );
+}
+
+export default App;
