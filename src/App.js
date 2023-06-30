2a006e325b0bcbc9fdf6a72a761b4b1f0b6e7a85
athiragithubworld <athirahrms@gmail.com>
2023-06-30 20:46:46 +0530

    App js - some updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, { useContext, useEffect } from "react";
 import LoginPage from "./Components/Authentication/LoginPage";
 import Header from "./Components/Layouts/Header";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import AuthContext from "./store/AuthContext";
 
 function App() {
+  const authcntx = useContext(AuthContext);
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   console.log(authcntx.isLoggedIn);
+  //   if (authcntx.isLoggedIn) {
+  //     navigate("/home");
+  //   }
+  // }, [authcntx.isLoggedIn]);
+
   return (
     <div>
       <Header></Header>
-      <LoginPage></LoginPage>
+      {!authcntx.isLoggedIn && <LoginPage></LoginPage>}
+      <Routes>
+        {/* {authcntx.isLoggedIn && <Route path="/home" element={<Header />} />} */}
+      </Routes>
     </div>
   );
 }
