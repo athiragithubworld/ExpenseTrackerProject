29b4428f7532644d34bb397de694ccb320865487
athiragithubworld <athirahrms@gmail.com>
2023-07-02 14:23:24 +0530

    Header js and css file (Task 3)

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../store/AuthContext";
-import classes from "./Header.module.css";
+// import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   const authcntx = useContext(AuthContext);
   const isLoggedIn = authcntx.isLoggedIn;
 
@@ -13,7 +13,7 @@ const Header = () => {
 
   return (
     <nav
-      className="navbar bg-body-tertiary"
+      className="navbar navbar-expand-lg bd-navbar sticky-top"
       style={{ backgroundColor: "black" }}
     >
       <div className="container-fluid">
@@ -25,23 +25,42 @@ const Header = () => {
               fontFamily: "Arila",
               fontWeight: "bold",
               fontStyle: "oblique",
-              marginLeft: 0,
-              textAlign: "left",
+              // marginLeft: 0,
+              // textAlign: "left",
             }}
           >
             {!isLoggedIn ? "Expense Tracker" : "Welcome to Expense Tracker"}
           </span>
         </div>
-        <ul>
-          {/* {!isLoggedIn && (
-            <li>
-              <Link to="/auth">Login</Link>
-            </li>
-          )} */}
-
+        <ul style={{ listStyle: "none" }}>
           {isLoggedIn && (
             <li>
-              <button onClick={LogoutHandler}>Logout</button>
+              <label
+                // className={classes.label}
+                style={{
+                  marginTop: "15px",
+                  color: "white",
+                  backgroundColor: "rgb(76, 18, 211)",
+                  borderRadius: "20px",
+                  padding: "5.5px",
+                  // gap: "10px",
+                }}
+              >
+                Your Profile is Incomplete.
+                <Link to="/contact" onClick={props.onClicks}>
+                  Complete Now
+                </Link>
+              </label>
+              <span style={{ marginLeft: "10px" }}>
+                <button
+                  style={{ borderRadius: "20px" }}
+                  type="button"
+                  className="btn btn-outline-danger"
+                  onClick={LogoutHandler}
+                >
+                  Logout
+                </button>
+              </span>
             </li>
           )}
         </ul>
