f64b92be7ef3a406cb4f6d2cc9fa971960b81b73
athiragithubworld <athirahrms@gmail.com>
2023-07-02 14:19:57 +0530

    App js - useNavigate (Task 3)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import LoginPage from "./Components/Authentication/LoginPage";
 import Header from "./Components/Layouts/Header";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import AuthContext from "./store/AuthContext";
+import ContactDetails from "./Components/Pages/ContactDetails";
 
 function App() {
   const authcntx = useContext(AuthContext);
-  // const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   console.log(authcntx.isLoggedIn);
-  //   if (authcntx.isLoggedIn) {
-  //     navigate("/home");
-  //   }
-  // }, [authcntx.isLoggedIn]);
+  const [openCart, setOpenCart] = useState(false);
+
+  const clickOpenCart = () => {
+    setOpenCart(true);
+  };
+
+  const closeCart = () => {
+    setOpenCart(false);
+  };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // console.log(authcntx.isLoggedIn);
+    if (!authcntx.isLoggedIn) {
+      navigate("/");
+    }
+  }, [authcntx.isLoggedIn]);
 
   return (
     <div>
-      <Header></Header>
-      {!authcntx.isLoggedIn && <LoginPage></LoginPage>}
+      <Header onClicks={clickOpenCart}></Header>
+
       <Routes>
-        {/* {authcntx.isLoggedIn && <Route path="/home" element={<Header />} />} */}
+        {!authcntx.isLoggedIn && (
+          <Route path="/" element={<LoginPage />}></Route>
+        )}
+        {authcntx.isLoggedIn && openCart && (
+          <Route
+            path="/contact"
+            element={<ContactDetails onClose={closeCart} />}
+          />
+        )}
       </Routes>
     </div>
   );
