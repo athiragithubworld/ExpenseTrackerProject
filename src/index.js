0d757b3f938cf2102c35eaba0d4ff85c3bdfb4a4
athiragithubworld <athirahrms@gmail.com>
2023-07-06 22:52:33 +0530

    index js (Task 8 - Wrapped by CartProvider)

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import AuthProvider from "./store/AuthProvider";
+import CartProvider from "./store/CartProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </BrowserRouter>
 );
