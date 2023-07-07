bdd708c0362b43a09e7db2ba44c4c3cd8031565c
athiragithubworld <athirahrms@gmail.com>
2023-07-07 22:09:00 +0530

    CartProvider js (Task - 9 - Store and get data from firebase)

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,11 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CartContext from "./CartContext";
+import axios from "axios";
 
 const CartProvider = (props) => {
   // console.log("object", props);
   const [expensesList, setExpensesList] = useState([]);
 
+  useEffect(() => {
+    // get the expense data from firebase
+    axios
+      .get(
+        "https://expense-tracker-project-64558-default-rtdb.firebaseio.com/dailyExepense.json"
+      )
+      .then((response) => {
+        console.log("get response", response);
+        const data = response.data;
+        const loadedMovies = [];
+        for (const key in data) {
+          loadedMovies.push({
+            id: key,
+            expenseKey: data[key].expenseKey,
+            expenseAmount: data[key].expenseAmount,
+            expenseDescription: data[key].expenseDescription,
+            expenseCategory: data[key].expenseCategory,
+          });
+        }
+        setExpensesList(loadedMovies);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }, []);
+
   const addExpenseHandler = (expenseItem) => {
+    // store data in firebase
+
+    axios
+      .post(
+        "https://expense-tracker-project-64558-default-rtdb.firebaseio.com/dailyExepense.json",
+        expenseItem
+      )
+      .then((response) => {
+        console.log("res", response.data);
+        //    const data =  response.data;
+        // const loadedMovies = {};
+        // for (const key in data) {
+        //   loadedMovies.push({
+        //     id: key,
+        //     title: data[key].title,
+        //     openingText: data[key].openingText,
+        //     releaseDate: data[key].releaseDate,
+        //   });
+        // }
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+
     setExpensesList([...expensesList, expenseItem]);
   };
 
