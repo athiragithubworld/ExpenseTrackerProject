d391e1eabfa6118ffc146d9709d20832a8a7a717
athiragithubworld <athirahrms@gmail.com>
2023-07-09 13:41:38 +0530

    CartContext and CartProvider (Task - 10 -- Edit and Delete Expense)

--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -4,6 +4,7 @@ const CartContext = React.createContext({
   expenseList: [],
   totalExpense: 0,
   addExpense: (item) => {},
+  removeExpense: (item) => {},
 });
 
 export default CartContext;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -20,6 +20,7 @@ const CartProvider = (props) => {
           loadedMovies.push({
             id: key,
             expenseKey: data[key].expenseKey,
+            expenseDate: data[key].expenseDate,
             expenseAmount: data[key].expenseAmount,
             expenseDescription: data[key].expenseDescription,
             expenseCategory: data[key].expenseCategory,
@@ -33,38 +34,84 @@ const CartProvider = (props) => {
   }, []);
 
   const addExpenseHandler = (expenseItem) => {
-    // store data in firebase
+    //  store data in firebase
+    console.log("expenseItem", expenseItem);
+    if (expenseItem.objId) {
+      axios
+        .put(
+          `https://expense-tracker-project-64558-default-rtdb.firebaseio.com/dailyExepense/${expenseItem.objId}.json`,
+          {
+            expenseDate: expenseItem.expenseDate,
+            expenseAmount: expenseItem.expenseAmount,
+            expenseDescription: expenseItem.expenseDescription,
+            expenseCategory: expenseItem.expenseCategory,
+            expenseKey: expenseItem.expenseKey,
+          }
+        )
+        .then((response) => {
+          console.log("put response ", response);
 
-    axios
-      .post(
-        "https://expense-tracker-project-64558-default-rtdb.firebaseio.com/dailyExepense.json",
-        expenseItem
-      )
-      .then((response) => {
-        console.log("res", response.data);
-        //    const data =  response.data;
-        // const loadedMovies = {};
-        // for (const key in data) {
-        //   loadedMovies.push({
-        //     id: key,
-        //     title: data[key].title,
-        //     openingText: data[key].openingText,
-        //     releaseDate: data[key].releaseDate,
-        //   });
-        // }
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
-
-    setExpensesList([...expensesList, expenseItem]);
+          const updateList = expensesList.filter(
+            (pdt) => pdt.id !== expenseItem.objId
+          );
+          console.log("put updatelist ", updateList);
+          const sortList = [
+            ...updateList,
+            { ...response.data, id: expenseItem.objId },
+          ];
+          console.log("sort", sortList.sort());
+          setExpensesList(sortList.sort());
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      axios
+        .post(
+          "https://expense-tracker-project-64558-default-rtdb.firebaseio.com/dailyExepense.json",
+          expenseItem
+        )
+        .then((response) => {
+          console.log("res", response.data);
+          //    const data =  response.data;
+          // const loadedMovies = {};
+          // for (const key in data) {
+          //   loadedMovies.push({
+          //     id: key,
+          //     title: data[key].title,
+          //     openingText: data[key].openingText,
+          //     releaseDate: data[key].releaseDate,
+          //   });
+          // }
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+      // getExpense();
+      setExpensesList([...expensesList, expenseItem]);
+    }
   };
 
-  // console.log("explist", expensesList);
+  const removeExpenseHandler = (expenseItem) => {
+    axios
+      .delete(
+        `https://expense-tracker-project-64558-default-rtdb.firebaseio.com/dailyExepense/${expenseItem.id}.json`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    const updateList = expensesList.filter((pdt) => pdt.id !== expenseItem.id);
+    setExpensesList(updateList);
+  };
+
   const expenseItem = {
     expenseList: expensesList,
     totalExpense: 0,
     addExpense: addExpenseHandler,
+    removeExpense: removeExpenseHandler,
   };
 
   return (
