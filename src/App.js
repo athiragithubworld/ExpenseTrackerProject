d00174ddf3beeaa2ae77da579a5484cff512fb4a
athiragithubworld <athirahrms@gmail.com>
2023-07-22 22:24:44 +0530

    App js (Task - 16)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,56 @@ import React, { useContext, useEffect, useState } from "react";
 import LoginPage from "./Components/Authentication/LoginPage";
 import Header from "./Components/Layouts/Header";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import AuthContext from "./store/AuthContext";
+import AuthContext from "./store/context/AuthContext";
 import ContactDetails from "./Components/Pages/ContactDetails";
 import DailyExpense from "./Components/Pages/DailyExpense";
+import { useDispatch, useSelector } from "react-redux";
+import { expenseActions } from "./store/ExpenseSlice";
+import axios from "axios";
 
 function App() {
-  const authcntx = useContext(AuthContext);
+  const auth = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  // console.log("slice loggin", isLoggedIn);
+
+  // const authcntx = useContext(AuthContext);
+
+  const onRestore = () => {
+    axios
+      .get(
+        `https://expense-tracker-project-64558-default-rtdb.firebaseio.com/dailyExepense/${auth.email}.json`
+      )
+      .then((response) => {
+        console.log("get response", response);
+        const data = response.data;
+        const loadedExpense = [];
+        for (const key in data) {
+          loadedExpense.push({
+            id: key,
+            expenseKey: data[key].expenseKey,
+            expenseDate: data[key].expenseDate,
+            expenseAmount: data[key].expenseAmount,
+            expenseDescription: data[key].expenseDescription,
+            expenseCategory: data[key].expenseCategory,
+          });
+        }
+        // const data = response.data;
+        // const dataList = Object.values(data);
+        // console.log("get data app", loadedExpense);
+        dispatch(expenseActions.onRestoreExpenses(loadedExpense));
+        // setExpensesList(loadedExpense);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  useEffect(() => {
+    if (auth.userLoggedIn) {
+      onRestore();
+    }
+  }, []);
 
   const [openCart, setOpenCart] = useState(false);
 
@@ -23,26 +67,26 @@ function App() {
 
   useEffect(() => {
     // console.log(authcntx.isLoggedIn);
-    if (!authcntx.isLoggedIn) {
+    if (!auth.userLoggedIn) {
       navigate("/");
     }
-  }, [authcntx.isLoggedIn]);
+
+    onRestore();
+  }, [auth.userLoggedIn]);
 
   return (
     <div>
       <Header onClicks={clickOpenCart}></Header>
 
       <Routes>
-        {!authcntx.isLoggedIn && (
-          <Route path="/" element={<LoginPage />}></Route>
-        )}
-        {authcntx.isLoggedIn && openCart && (
+        {!auth.userLoggedIn && <Route path="/" element={<LoginPage />}></Route>}
+        {auth.userLoggedIn && openCart && (
           <Route
             path="/contact"
             element={<ContactDetails onClose={closeCart} />}
           />
         )}
-        {authcntx.isLoggedIn && (
+        {auth.userLoggedIn && (
           <Route path="/dailyexpense" element={<DailyExpense />} />
         )}
       </Routes>
