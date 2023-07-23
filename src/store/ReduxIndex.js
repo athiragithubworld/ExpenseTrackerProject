9bae16af252712263b53ee225fe17c94413f6a14
athiragithubworld <athirahrms@gmail.com>
2023-07-23 23:20:20 +0530

    ReduxIndex js (Task -17) add themeSlice in reduxstore

--- src/store/ReduxIndex.js
+++ src/store/ReduxIndex.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./AuthSlice";
 import expenseReducer from "./ExpenseSlice";
+import themeReducer from "./ThemeSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     expenses: expenseReducer,
+    themeMode: themeReducer,
   },
 });
 
